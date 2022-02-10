3023bbf96840b46edd6751bcce9a240577e9869d
Henry Roberts <whroberts5@gmail.com>
2022-02-10 11:48:09 -0600

    base towers class

---
+++ Assets/Scripts/Towers/Tower1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower1 : TowerBase
+{
+    public override void PrintOnStart()
+    {
+        Debug.Log("Printing From Override function in Tower1 in Start");
+    }
+
+    private void Start()
+    {
+        PrintOnStart();
+    }
+}
---
+++ Assets/Scripts/Towers/TowerBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TowerBase : MonoBehaviour
+{
+    public abstract void PrintOnStart();
+
+    private void Awake()
+    {
+        Debug.Log("Print From Base Class In Awake");
+    }
+}
