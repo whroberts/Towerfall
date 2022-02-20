77954216dad18a9fdaa5b9df54aa9c02bb288181
Mikey <mikeybess27@gmail.com>
2022-02-19 20:24:10 -0600

    Added art and default tower values

--- Assets/Scripts/Towers/TowerBase.cs
+++ Assets/Scripts/Towers/TowerBase.cs
@@ -2,8 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
 public abstract class TowerBase : MonoBehaviour, IDamagable
 {
+    [Header("Tower Base Stats")]
+
+    // Shots per second
+    [SerializeField] protected float _towerFireRate = 2;
+
+    // Automatically reloads
+    [SerializeField] protected float _currentAmmo = 0;
+    [SerializeField] protected float _totalAmmo = 10;
+
+    // In seconds
+    [SerializeField] protected float _reloadTime = 1;
+
+    // Multiple of normal gravity
+    [SerializeField] protected float _towerGravity = 2;
+
+    [SerializeField] protected int _currentHealth = 0;
+    [SerializeField] protected int _totalHealth = 25;
+
+    Collider2D _col;
+    Rigidbody2D _rb;
+
+    void Awake()
+    {
+        _col = GetComponent<Collider2D>();
+        _rb = GetComponent<Rigidbody2D>();
+
+        _currentHealth = _totalHealth;
+        _currentAmmo = _totalAmmo;
+    }
+
     public abstract void PrintOnStart();
     public abstract void Shoot();
 
@@ -14,6 +47,12 @@ public abstract class TowerBase : MonoBehaviour, IDamagable
 
     public void TakeDamage(int damage)
     {
+        _currentHealth = -damage;
 
+        if (_currentHealth <= 0)
+        {
+            Debug.Log(this.name + "has taken fatal damage");
+            Destroy(gameObject);
+        }
     }
 }
