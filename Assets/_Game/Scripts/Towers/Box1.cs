4dbef7b84774e4dfc0409b619f5bb0ed16e75345
Mikey <mikeybess27@gmail.com>
2022-03-02 17:51:00 -0600

    Added new tower stuff
    Test version of new sniper tower, added in two boxes, one heavy one scaffold (Enemies can attack them). Tried messing with enemy detection for the nail tower. Sniper tower has blob projectile

---
+++ Assets/_Game/Scripts/Towers/Box1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box1 : TowerBase
+{
+
+    public override void PrintOnStart()
+    {
+        Debug.Log("Printing From Override function in Box1 in Start");
+    }
+
+    public override void Shoot()
+    {
+
+    }
+
+    public override IEnumerator Reload()
+    {
+        yield return new WaitForSeconds(_reloadTime);
+    }
+
+    private void Update()
+    {
+
+    } 
+
+    private void Start()
+    {
+
+    }
+}
