35637f525c70d6ade8e8cdcb9d696b384783dbe2
Nick Young <nty170230@utdallas.edu>
2022-03-24 05:54:50 -0500

    music stuff 2
    added an audio manager

---
+++ Assets/_Game/Scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioClip _musicGame = null;
+    public static AudioManager instance = null;
+
+    AudioSource _audioSource;
+
+    private void Awake()
+    {
+            if(_musicGame != null)
+        {
+            AudioManager.instance.PlaySong(_musicGame);
+        }
+    }
+
+    public void PlaySong(AudioClip clip)
+    {
+        _audioSource.clip = clip;
+        _audioSource.Play();
+    }
+}
