9a64b418d64fa38499d72250acf35534d701de96
Nick Young <nty170230@utdallas.edu>
2022-03-24 02:21:21 -0500

    enemy sprite animations
    added alternate prefabs of enemies with animations

---
+++ Assets/_Game/Scripts/SpriteRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteRotation : MonoBehaviour
+{
+    // Use this for initialization
+    public float speed = 60f;
+    public float rotationAngle = 30f;
+    public float rotationOffset = 0f;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localEulerAngles = new Vector3(0, 0, Mathf.PingPong(Time.time*speed, rotationAngle) - rotationOffset);
+    }
+}
