f6010f516d16c1463a74ef3029fba9f6b8727633
James <ninjabobtwo@gmail.com>
2022-03-23 23:54:45 -0500

    drag and drop towers

--- Assets/_Game/Scripts/Managers/TowerManager.cs
+++ Assets/_Game/Scripts/Managers/TowerManager.cs
@@ -8,15 +8,17 @@ public class TowerManager : MonoBehaviour
 {
     [SerializeField] private GameObject _basicTower = null;
     [SerializeField] private GameObject _sniperTower = null;
+    [SerializeField] private GameObject _boxTower = null;
 
     private bool _holdingTower = false;
 
     private GameObject _newTower;
+    private int _newTowerType;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _newTowerType = 0;
     }
 
     // Update is called once per frame
@@ -30,27 +32,23 @@ public class TowerManager : MonoBehaviour
                 Vector3 pos = Camera.main.ScreenToWorldPoint(touch.position);
                 pos.z = 0;
 
-                //Temporary for alpha - only place the tower below the ui
-                if (pos.y <= 23)
+                if (touch.phase == TouchPhase.Began)
                 {
-                    if (touch.phase == TouchPhase.Began)
-                    {
-                        _newTower = Instantiate(_basicTower, pos, Quaternion.identity);
-                        _newTower.GetComponent<Rigidbody2D>().freezeRotation = true;
-                        _holdingTower = true;
+                    _newTower = Instantiate(_basicTower, pos, Quaternion.identity);
+                    _newTower.GetComponent<Rigidbody2D>().freezeRotation = true;
+                    _holdingTower = true;
 
-                    }
-                    else if (touch.phase == TouchPhase.Moved)
-                    {
-                        _newTower.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
-                        _newTower.transform.position = pos;
-                    }
-                    else if (touch.phase == TouchPhase.Ended)
-                    {
-                        _holdingTower = false;
-                        _newTower.GetComponent<Rigidbody2D>().freezeRotation = false;
-                        _newTower.GetComponent<TowerBase>()?.SetGravity(_newTower.GetComponent<TowerBase>().TowerGravity);
-                    }
+                }
+                else if (touch.phase == TouchPhase.Moved)
+                {
+                    _newTower.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
+                    _newTower.transform.position = pos;
+                }
+                else if (touch.phase == TouchPhase.Ended)
+                {
+                    _holdingTower = false;
+                    _newTower.GetComponent<Rigidbody2D>().freezeRotation = false;
+                    _newTower.GetComponent<TowerBase>()?.SetGravity(_newTower.GetComponent<TowerBase>().TowerGravity);
                 }
             }
             else
@@ -58,32 +56,42 @@ public class TowerManager : MonoBehaviour
                 Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 pos.z = 0;
 
-                //Temporary for alpha - only place the tower below the ui
-                if (pos.y <= 23)
+                if (Input.GetMouseButtonDown(0) && !_holdingTower && _newTowerType != 0)
                 {
-                    if (Input.GetMouseButtonDown(0) && !_holdingTower)
-                    {
-                        _newTower = Instantiate(_basicTower, pos, Quaternion.identity);
-                        _newTower.GetComponent<Rigidbody2D>().freezeRotation = true;
-                        _holdingTower = true;
-                    }
+                    if (_newTowerType == 1) _newTower = Instantiate(_basicTower, pos, Quaternion.identity);
+                    else if (_newTowerType == 2) _newTower = Instantiate(_sniperTower, pos, Quaternion.identity);
+                    else if (_newTowerType == 3) _newTower = Instantiate(_boxTower, pos, Quaternion.identity);
 
-                    if (_holdingTower)
-                    {
-                        _newTower.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
-                        _newTower.transform.position = pos;
-                    }
-
-                    if (Input.GetMouseButtonUp(0))
-                    {
-                        _holdingTower = false;
-                        _newTower.GetComponent<Rigidbody2D>().freezeRotation = false;
-                        _newTower.GetComponent<TowerBase>()?.SetGravity(_newTower.GetComponent<TowerBase>().TowerGravity);
-                    }
+                    _newTower.GetComponent<Rigidbody2D>().freezeRotation = true;
+                    _holdingTower = true;
                 }
-                
+
+                if (_holdingTower)
+                {
+                    _newTower.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
+                    _newTower.transform.position = pos;
+                }
+
+                if (Input.GetMouseButtonUp(0))
+                {
+                    _holdingTower = false;
+                    _newTower.GetComponent<Rigidbody2D>().freezeRotation = false;
+                    _newTower.GetComponent<TowerBase>()?.SetGravity(_newTower.GetComponent<TowerBase>().TowerGravity);
+                }                
             }
         }
         
     }
+
+    public void TowerButtonPressEvent(int towerType)
+    {
+        _newTowerType = towerType;
+    }
+
+    public void TowerButtonReleaseEvent()
+    {
+        _newTowerType = 0;
+    }
+
+
 }
